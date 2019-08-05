b433cf69a6ceeeb933b9c6bdd458fa53823c116a
vanhngyen <53243467+vanhngyen@users.noreply.github.com>
2019-08-05 20:17:17 +0700

    bt_ontap

---
+++ bton_b1.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+	int n,m;
+		printf("nhap so n:\n");
+		scanf("%d",&n);
+		printf("nhap so m:\n");
+		scanf("%d",&m);
+		
+		if(m>n){
+			printf("so lon hon la:%d\n",m);
+		}else{
+			printf("so lon hon la:%d\n",n);
+		} 
+	 
+	return 0;
+}
---
+++ bton_b2.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+	int n;
+		printf("nhap so n:\n");
+		scanf("%d",&n);
+		
+	if(n<0){
+		printf("so n am :%d ",n);
+	}else{
+		printf("so n duong :%d",n);
+	}
+	return 0;
+}
